e8faddf6acfc8256c6b648ac11206f5275a19dcd
Vinicius Gabriel Fonseca <vinicius.fonseca20093004@gmail.com>
2022-06-04 00:16:27 -0300

    Adicionando o Yup para validar as informações dos usuarios

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,9 +1,30 @@
 import { v4 } from 'uuid'
+import * as Yup from 'yup'
 
 import User from '../models/User'
 class UserController {
   async store (request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      cpf: Yup.string().required(),
+      telefone: Yup.string().required(),
+      email: Yup.string().email().required()
+    })
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+
     const { name, cpf, telefone, email } = request.body
+
+    const userExists = await User.findOne({
+      where: { email }
+    })
+    if (userExists) {
+      return response.status(409).json({ error: 'User already exists' })
+    }
 
     const user = await User.create({
       id: v4(),
@@ -13,7 +34,7 @@ class UserController {
       email
     })
 
-    return response.json(user)
+    return response.status(201).json({ id: user.id, name, email })
   }
 }
 export default new UserController()
